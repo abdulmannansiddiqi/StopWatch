4d0f5fd04781da8467e61e54024036ede946b072
abdulmannansiddiqi <abdulmannansiddiqi7@gmail.com>
2025-07-08 23:48:31 +0500

    initial commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+let [seconds,minutes,hours] = [0,0,0]
+// let displayTime =document.getElementById('displayTime')
+// let timer = null
+// function stopWatch(){
+//     seconds++ 
+//     if(seconds === 60){
+//         seconds=0
+//         minutes++
+
+//         if(minutes === 60){
+//             minutes=0
+//             hours++
+//         }
+//     }
+// }
+// watchStart(()=>{
+//     if(timer !== null)
+//     timer = setInterval(1000,stopWatch)
+// })
