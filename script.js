288896c5c5340d7c0ccb364bf1cb2751f1917dd7
abdulmannansiddiqi <abdulmannansiddiqi7@gmail.com>
2025-07-10 23:29:50 +0500

    review it

--- script.js
+++ script.js
@@ -1,40 +1,38 @@
-let [seconds,minutes,hours] = [0,0,0]
-let displayTime =document.getElementById('displayTime')
+let [seconds, minutes, hours] = [0, 0, 0]
+let displayTime = document.getElementById('displayTime')
 let timer = null
-function stopWatch(){
-    seconds++ 
-    if(seconds === 60){
-        seconds=0
+function stopWatch() {
+    seconds++
+    if (seconds === 60) {
+        seconds = 0
         minutes++
 
-        if(minutes === 60){
-            minutes=0
+        if (minutes === 60) {
+            minutes = 0
             hours++
         }
     }
-    let h = hours<10 ? '0' + hours:hours;
-    let m = minutes<10 ? '0' + minutes:minutes;
-    let s = seconds<10 ? '0' + seconds:seconds;
-    
-    displayTime.innerHTML= h + ':' + m + ':' + s
+    let h = hours < 10 ? '0' + hours : hours;
+    let m = minutes < 10 ? '0' + minutes : minutes;
+    let s = seconds < 10 ? '0' + seconds : seconds;
+
+    displayTime.innerHTML = h + ':' + m + ':' + s
 }
-function watchStart(){
-    if(timer !== null){
+function watchStart() {
+    if (timer !== null) {
         clearInterval(timer)
     }
-    timer = setInterval(stopWatch,1000)
+    timer = setInterval(stopWatch, 1000)
 }
-function watchStop(){
+function watchStop() {
     clearInterval(timer)
 }
-function watchReset(){
+function watchReset() {
     clearInterval(timer)
-    displayTime.innerHTML="00:00:00";
-    [seconds,minutes,hours] = [0,0,0]
-    
-    
+    displayTime.innerHTML = "00:00:00";
+    [seconds, minutes, hours] = [0, 0, 0]
+
 }
-// const arr1 = [1,2,3]
 
 
 
